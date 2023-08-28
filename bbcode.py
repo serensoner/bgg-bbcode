4283cea31b74af7323f6537debd9e7edeb237c01
serensoner <seren.soner@gmail.com>
2023-08-28 18:50:34 +0200

    Update bbcode.py

--- bbcode.py
+++ bbcode.py
@@ -227,6 +227,7 @@ class Parser(object):
         self.add_simple_formatter("b", "<strong>%(value)s</strong>")
         self.add_simple_formatter("i", "<em>%(value)s</em>")
         self.add_simple_formatter("u", "<u>%(value)s</u>")
+        self.add_simple_formatter("-", "<strike>%(value)s</strike>")
         self.add_simple_formatter("s", "<strike>%(value)s</strike>")
         self.add_simple_formatter("hr", "<hr />", standalone=True)
         self.add_simple_formatter("sub", "<sub>%(value)s</sub>")
